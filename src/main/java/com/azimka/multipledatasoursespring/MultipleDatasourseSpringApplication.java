f6ca0ec0faf762dd0490b9c773df4169d260e91b
azimka <aszimka@gmail.com>
2019-01-19 22:39:27 +0300

    Initial commit

---
+++ src/main/java/com/azimka/multipledatasoursespring/MultipleDatasourseSpringApplication.java
@@ -0,0 +1,14 @@
+package com.azimka.multipledatasoursespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDatasourseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleDatasourseSpringApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/azimka/multipledatasoursespring/MultipleDatasourseSpringApplicationTests.java
@@ -0,0 +1,17 @@
+package com.azimka.multipledatasoursespring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultipleDatasourseSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
