2ea7a47736f6b76a56107b3c089bb2a1cbe2a2e5
azimka <aszimka@gmail.com>
2019-01-20 10:37:28 +0300

    Created multiple configuration classes

--- src/main/java/com/azimka/multipledatasoursespring/configuration/DB1Configuration.java
+++ src/main/java/com/azimka/multipledatasoursespring/configuration/DB1Configuration.java
@@ -1,10 +1,43 @@
 package com.azimka.multipledatasoursespring.configuration;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.azimka.multipledatasoursespring.db1")
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.azimka.multipledatasoursespring.db1.repository",
+        entityManagerFactoryRef = "db1EntityManagerFactory")
 public class DB1Configuration {
 
+    @Bean("db1DataSource")
+    @ConfigurationProperties(prefix = "db1.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean("db1EntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(EntityManagerFactoryBuilder builder, @Qualifier("db1DataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.azimka.multipledatasoursespring.db1.model")
+                .persistenceUnit("db1")
+                .build();
+    }
+
+    @Bean("db1TransactionManager")
+    public PlatformTransactionManager createTransactionManager(@Qualifier("db1EntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 }
--- src/main/java/com/azimka/multipledatasoursespring/configuration/DB2Configuration.java
+++ src/main/java/com/azimka/multipledatasoursespring/configuration/DB2Configuration.java
@@ -1,10 +1,45 @@
 package com.azimka.multipledatasoursespring.configuration;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.azimka.multipledatasoursespring.db2")
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.azimka.multipledatasoursespring.db2.repository",
+        repositoryImplementationPostfix = "db2EntityManagerFactory")
 public class DB2Configuration {
+
+    @Bean("db2DataSource")
+    @ConfigurationProperties(prefix = "db2.datasource")
+    public DataSource db2DataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean("db2EntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean createEntityManager(@Qualifier("db2DataSource") DataSource dataSource, EntityManagerFactoryBuilder builder){
+        return builder
+                .dataSource(dataSource)
+                .persistenceUnit("db2")
+                .packages("com.azimka.multipledatasoursespring.db2")
+                .build();
+    }
+
+    @Bean("db2TransactionManager")
+    public PlatformTransactionManager transactionManager(@Qualifier("db2EntityManagerFactory") EntityManagerFactory entityManagerFactory){
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 
 }
